04beddc91b865f341e1f310cd825479bb5148cee
Jacek Pietrzyk <jacpie3k@gmail.com>
2019-12-05 10:45:37 +0100

    Add simple products seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+
+Dir.chdir("./db")
+puts Dir.pwd
+Dir.each_child("products") do |file|
+  data = JSON.load(File.open("products/#{file}"))
+  Product.create(name: data["data"]["productDisplayName"], description: data["data"]["displayCategories"])
+  p "Product #{data["data"]["productDisplayName"]} added"
+end
