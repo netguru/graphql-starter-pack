68f3fe1882e4c08a748d047c285c72c2c8d37395
Jacek Pietrzyk <jacpie3k@gmail.com>
2019-12-05 12:38:06 +0100

    Add CORS

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,12 @@ module FashionStore
       g.helper          false
       g.channel         assets: false
     end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+    end
   end
 end
