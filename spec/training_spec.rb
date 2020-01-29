ea1aebfb9f4fb456bbad86ed87892686148308fd
Grzegorz Hauska <grzegorz.hauska@netguru.com>
2020-01-29 09:45:13 +0100

    clean up training file structure

--- spec/training_spec.rb
+++ spec/training_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe "graphql training", type: :request do
   # https://graphql-ruby.org/guides
   # 
 
+  # Topic checklist:
   # Types & relations
   # Mutations (create, update, delete)
   # Filters
@@ -22,144 +23,142 @@ RSpec.describe "graphql training", type: :request do
   # graphql-batch.
   # authentication
 
-  context "scenarios" do
-    context "queries" do
-      it "" do
-        shoes = ProductCategory.create!(name: "Shoes")
-        otwarty_nosek = Product.create!(name: "Otwarty nosek", price_cents: 1, product_category: shoes)
-        Product.create!(name: "Płaski obcas", price_cents: 1, product_category: shoes)
-        ProductVariant.create!(variant_type: "color", value: "ffffff", label: "white", product: otwarty_nosek)
-        ProductVariant.create!(variant_type: "color", value: "000000", label: "black", product: otwarty_nosek)
+  context "basic" do
+    it "queries" do
+      shoes = ProductCategory.create!(name: "Shoes")
+      otwarty_nosek = Product.create!(name: "Otwarty nosek", price_cents: 1, product_category: shoes)
+      Product.create!(name: "Płaski obcas", price_cents: 1, product_category: shoes)
+      ProductVariant.create!(variant_type: "color", value: "ffffff", label: "white", product: otwarty_nosek)
+      ProductVariant.create!(variant_type: "color", value: "000000", label: "black", product: otwarty_nosek)
 
-        # First lets make a simple query work.
-        # g-search "training_step_1"
-        # todo: comment out and mark types when all specs are finished
+      # First lets make a simple query work.
+      # g-search "training_step_1"
+      # todo: comment out and mark types when all specs are finished
 
-        query = 
-          %(query {
-              products {
-                name
-              }
+      query = 
+        %(query {
+            products {
+              name
             }
-          )
+          }
+        )
 
-        post "/graphql", params: { query: query }
+      post "/graphql", params: { query: query }
 
-        result = JSON.parse(response.body)
+      result = JSON.parse(response.body)
 
-        names = result.dig("data", "products").map { |product| product["name"] }
-        expect(names).to match_array(["Otwarty nosek", "Płaski obcas"])
+      names = result.dig("data", "products").map { |product| product["name"] }
+      expect(names).to match_array(["Otwarty nosek", "Płaski obcas"])
 
-        # Graphiql tool
-        # Now when you can run a simple query, go to http://localhost:3000/graphiql and query "{ products { name } }" .
-        # This is a console to interact directly with graphql engine, very helpfull during development.
-        # graphiql_works_for_me = false
-        # expect(graphiql_works_for_me).to be true
+      # Graphiql tool
+      # Now when you can run a simple query, go to http://localhost:3000/graphiql and query "{ products { name } }" .
+      # This is a console to interact directly with graphql engine, very helpfull during development.
+      # graphiql_works_for_me = false
+      # expect(graphiql_works_for_me).to be true
 
-        query =
-          %(query {
-              productCategory(id: #{shoes.id}) {
+      query =
+        %(query {
+            productCategory(id: #{shoes.id}) {
+              name
+              products(name: "Otwarty nosek") {
                 name
-                products(name: "Otwarty nosek") {
-                  name
-                  productVariants {
-                    label
-                  }
+                productVariants {
+                  label
                 }
               }
-            })
-        
-        post "/graphql", params: { query: query }
+            }
+          })
+      
+      post "/graphql", params: { query: query }
 
-        result = JSON.parse(response.body)
+      result = JSON.parse(response.body)
 
-        labels = result.dig("data", "productCategory", "products")
-                      .flat_map { |product| product["productVariants"] }
-                      .flat_map { |variant| variant["label"] } 
-        expect(labels).to match_array(["white", "black"])
+      labels = result.dig("data", "productCategory", "products")
+                    .flat_map { |product| product["productVariants"] }
+                    .flat_map { |variant| variant["label"] } 
+      expect(labels).to match_array(["white", "black"])
 
-        # todo:
-        # pagination
-        # filters
-        # graphql-batch.
-        # operation name
-        # variables
-        # https://graphql.org/learn/queries
-      end
-
-      context "optimized" do
-        it "use DataLoader to fix n+1" do
-          # DataLoader — Source code walkthrough https://www.youtube.com/watch?v=OQTnXNCDywA
-          pending
-        end
-      end
+      # todo:
+      # pagination
+      # filters
+      # graphql-batch.
+      # operation name
+      # variables
+      # https://graphql.org/learn/queries
     end
 
-    context "mutations" do
-      it "" do
-        cart = Cart.create!(number_of_items: 0)
-        shoes = ProductCategory.create!(name: "Shoes")
-        otwarty_nosek = Product.create!(name: "Otwarty nosek", price_cents: 1, product_category: shoes)
-        black_variant = ProductVariant.create!(variant_type: "color", value: "000000", label: "black", product: otwarty_nosek)
-        white_variant = ProductVariant.create!(variant_type: "color", value: "ffffff", label: "white", product: otwarty_nosek)
+    it "mutations" do
+      cart = Cart.create!(number_of_items: 0)
+      shoes = ProductCategory.create!(name: "Shoes")
+      otwarty_nosek = Product.create!(name: "Otwarty nosek", price_cents: 1, product_category: shoes)
+      black_variant = ProductVariant.create!(variant_type: "color", value: "000000", label: "black", product: otwarty_nosek)
+      white_variant = ProductVariant.create!(variant_type: "color", value: "ffffff", label: "white", product: otwarty_nosek)
 
-        query = %(
+      query = %(
+        mutation {
+          createCartItem(input: {productId: #{otwarty_nosek.id},
+                                productVariantId: #{white_variant.id},
+                                quantity: 2} ) {
+            cartItem {
+              id
+              quantity
+              productVariant {
+                id
+              }
+            }
+            errors
+          }
+        }
+        )
+
+      expect(CartItem.count).to eq 0
+
+      # create
+      post "/graphql", params: { query: query }
+
+      expect(CartItem.count).to eq 1
+      cart_item = CartItem.first!
+      expect(cart_item.product).to eq otwarty_nosek
+      expect(cart_item.product_variant).to eq white_variant
+      expect(cart_item.quantity).to eq 2
+
+      query = %(
           mutation {
-            createCartItem(input: {productId: #{otwarty_nosek.id},
-                                  productVariantId: #{white_variant.id},
-                                  quantity: 2} ) {
+            updateCartItem(input: {id: #{cart_item.id},
+                                  quantity: 4 } ) {
               cartItem {
                 id
                 quantity
-                productVariant {
-                  id
-                }
               }
               errors
             }
-          }
-          )
+          })
 
-        expect(CartItem.count).to eq 0
+      # update
+      post "/graphql", params: { query: query }
 
-        # create
-        post "/graphql", params: { query: query }
+      cart_item.reload
+      expect(cart_item.quantity).to eq 4
 
-        expect(CartItem.count).to eq 1
-        cart_item = CartItem.first!
-        expect(cart_item.product).to eq otwarty_nosek
-        expect(cart_item.product_variant).to eq white_variant
-        expect(cart_item.quantity).to eq 2
+      query = %(
+          mutation {
+            destroyCartItem(input: {id: #{cart_item.id}} ) {
+              errors
+            }
+          })
 
-        query = %(
-            mutation {
-              updateCartItem(input: {id: #{cart_item.id},
-                                    quantity: 4 } ) {
-                cartItem {
-                  id
-                  quantity
-                }
-                errors
-              }
-            })
+      # destroy
+      post "/graphql", params: { query: query }
 
-        # update
-        post "/graphql", params: { query: query }
+      expect(CartItem.count).to eq 0
+    end
+  end
 
-        cart_item.reload
-        expect(cart_item.quantity).to eq 4
-
-        query = %(
-            mutation {
-              destroyCartItem(input: {id: #{cart_item.id}} ) {
-                errors
-              }
-            })
-
-        # destroy
-        post "/graphql", params: { query: query }
-
-        expect(CartItem.count).to eq 0
+  context "advanced" do
+    context "optimizing queries" do
+      it "use DataLoader to fix n+1" do
+        # DataLoader — Source code walkthrough https://www.youtube.com/watch?v=OQTnXNCDywA
+        pending
       end
     end
 
