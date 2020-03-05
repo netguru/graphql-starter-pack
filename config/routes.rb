8ba6c903428a7289057eba74e4cadb1f256fa17d
Hubert Jakubiak <hubert.jakubiak@netguru.co>
2020-03-05 14:16:13 +0100

    Add comment about post endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,14 @@ Rails.application.routes.draw do
   root to: "graphql#index"
 
   # scenario_1
+  #
+  # Notice:
+  # In GraphQL, you'll provide a JSON-encoded body whether
+  # you're performing a query or a mutation, so the HTTP verb is POST.
+  # It's possible to be done with GET verb too, but the disadvantage is that
+  # you need to send a query or a mutation in a query string as part of an URL.
+  # GitHub, Shopify use only POST endpoints for that purpose in their APIs.
+  #
   # post "/graphql", to: "graphql#execute"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
